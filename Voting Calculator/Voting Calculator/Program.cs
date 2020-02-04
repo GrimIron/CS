bc68dbd3a4c6bd5b9964c192dc159f1eb5899fa8
GrimIron <60586368+GrimIron@users.noreply.github.com>
2020-02-04 14:30:25 +0000

    Main

---
+++ Voting Calculator/Voting Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Voting_Calculator
+{
+    class Country_data
+    {
+        public static void Main()
+        {
+            string country = System.IO.File.ReadAllText(@"C:\Users\ODSTc\source\repos\Voting Calculator\Voting Calculator\Country Data.txt");
+            Console.WriteLine("Contents of Country Data.txt = {0}", country);
+
+        }
+    }
+
+}
