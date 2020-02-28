a70ad626bfb003bd5e7f0d1a850fec6412d6b518
Callum Cross <40017446@edu.ucnl.ac.uk>
2020-02-28 12:18:31 +0000

    Algorithm Lesson 5 - Sort example

---
+++ Algo 5/Algo 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myint = genarate_randon_array(256, 0, 2048);
+
+            Sort.mergeSort(myint, 0, myint.Length - 1);
+
+            for (int i = 0; i < myint.Length; i++)
+            {
+                Console.Write(myint[i] + ",");
+            }
+            Console.ReadLine();
+        }
+
+        public static int[] genarate_randon_array(int size, int min, int max)
+        {
+            Random r = new Random();
+            int[] myarr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                myarr[i] = r.Next(min, max + 1);
+            }
+
+            return myarr;
+        }
+    }
+}
